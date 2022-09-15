d8515559a5582209fa0bca4d5238741458f202f2
Caleb <calebjoshuapaul@gmail.com>
2022-09-15 13:52:21 +0530

    added Menu component and styles with toggle theme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/Theme/Theme";
 import Menu from "./components/Menu/Menu";
 import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
 `;
-const Main = styled.div``;
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
 const Wrapper = styled.div``;
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <Container className="App">
-      <Menu />
-      <Main>
-        <Navbar />
-        <Wrapper>video cards</Wrapper>
-      </Main>
-    </Container>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container className="App">
+        <Menu setDarkMode={setDarkMode} />
+        <Main>
+          <Navbar />
+          <Wrapper>video cards</Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,170 @@
 import React from "react";
+import YouTubeLogo from "../../assets/YouTubeLogo.png";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import HistoryIcon from "@mui/icons-material/History";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import MovieCreationIcon from "@mui/icons-material/MovieCreation";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ReportIcon from "@mui/icons-material/Report";
+import HelpIcon from "@mui/icons-material/Help";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
-function Menu() {
-  return <div>Menu</div>;
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+  background: ${({ theme }) => theme.bg};
+  height: 100vh;
+  color: ${({ theme }) => theme.text};
+  font-size: 14px;
+  position: sticky;
+  top: 0;
+  overflow-x: visible;
+  overflow-y: auto;
+`;
+
+const Wrapper = styled.div`
+  padding: 18px 26px;
+`;
+
+const Logo = styled.div`
+  align-items: center;
+  display: flex;
+  font-weight: bold;
+  gap: 5px;
+  margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+  background-color: #ff0000;
+  border-radius: 6px;
+  padding: 0 3px;
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Login = styled.div``;
+const Button = styled.div`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5;
+`;
+
+function Menu({ setDarkMode }) {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={YouTubeLogo} />
+          SupTube
+        </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <ExploreIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsIcon />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <VideoLibraryIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryIcon />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <Button>
+            <AccountCircleOutlinedIcon />
+            SIGN IN
+          </Button>
+        </Login>
+        <Hr />
+        <Item>
+          <LibraryMusicIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsIcon />
+          Gaminig
+        </Item>
+        <Item>
+          <MovieCreationIcon />
+          Movies
+        </Item>
+        <Item>
+          <NewspaperIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvIcon />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsIcon />
+          Settings
+        </Item>
+        <Item>
+          <ReportIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpIcon />
+          Help
+        </Item>
+        <Item
+          onClick={() => {
+            setDarkMode((darkMode) => !darkMode);
+          }}
+        >
+          <LightModeIcon />
+          Light Mode
+        </Item>
+      </Wrapper>
+    </Container>
+  );
 }
 
 export default Menu;
