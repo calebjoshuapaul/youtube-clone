e78a3ce5315c989e2d0f10cc3a8c7b6ca0788a14
Caleb <calebjoshuapaul@gmail.com>
2022-09-15 12:46:53 +0530

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Menu from "./components/Menu/Menu";
+import Navbar from "./components/Navbar/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div``;
+const Wrapper = styled.div``;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>video cards</Wrapper>
+      </Main>
+    </Container>
   );
 }
 
